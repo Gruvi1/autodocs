17c47edf5cac7b315213625d33d33178efa4b7a0
Gruvi1 <astakxov134@gmail.com>
2025-11-15 19:39:49 +0700

    credentialsPath has been converted to a resource

--- src/main/java/ru/nsu/astakhov/autodocs/integration/google/GoogleSheetsConfig.java
+++ src/main/java/ru/nsu/astakhov/autodocs/integration/google/GoogleSheetsConfig.java
@@ -9,17 +9,23 @@ import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class GoogleSheetsConfig {
+    private final ResourceLoader resourceLoader;
+
     @Value("${google.sheets.credential.path}")
     private String credentialsPath;
 
@@ -28,16 +34,19 @@ public class GoogleSheetsConfig {
 
     @Bean
     public Sheets googleSheets() throws GeneralSecurityException, IOException {
-        GoogleCredentials credentials = ServiceAccountCredentials
-                .fromStream(new FileInputStream(credentialsPath))
-                .createScoped(List.of(SheetsScopes.SPREADSHEETS));
+        Resource resource = resourceLoader.getResource(credentialsPath);
+        try (InputStream inputStream = resource.getInputStream()) {
+            GoogleCredentials credentials = ServiceAccountCredentials
+                    .fromStream(inputStream)
+                    .createScoped(List.of(SheetsScopes.SPREADSHEETS));
 
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+            HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
 
-        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                                GsonFactory.getDefaultInstance(),
-                                requestInitializer)
-                .setApplicationName(applicationName)
-                .build();
+            return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                    GsonFactory.getDefaultInstance(),
+                    requestInitializer)
+                    .setApplicationName(applicationName)
+                    .build();
+        }
     }
 }
