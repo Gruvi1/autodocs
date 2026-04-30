3159a8b22f9b52750f348c934bce4e5b2d42f8f0
Gruvi1 <astakxov134@gmail.com>
2026-04-30 18:07:13 +0700

    bugfix

--- src/main/java/ru/nsu/astakhov/autodocs/document/generator/table/ThesisFrontPageTableProcessor.java
+++ src/main/java/ru/nsu/astakhov/autodocs/document/generator/table/ThesisFrontPageTableProcessor.java
@@ -1,5 +1,7 @@
 package ru.nsu.astakhov.autodocs.document.generator.table;
 
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
@@ -7,37 +9,28 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
 public class ThesisFrontPageTableProcessor extends TableProcessor {
     public void addCoSupervisor(XWPFTable table) {
-        XWPFTableRow row1 = removeAllCells(table.createRow());
-        row1.createCell();
-        XWPFTableCell row1Cell2 = row1.createCell();
-        addTextInCell(row1Cell2, "Соруководитель ВКР");
+        int fontSize = 12;
+        XWPFRun run1 = addRow(table, "Соруководитель ВКР", fontSize, false);
+        run1.setBold(true);
+        addRow(table, "$(соруководительВКР.степень) $(соруководительВКР.звание)", fontSize, true);
+        addRow(table, "$(соруководительВКР.должность.работаНГУ)", fontSize, true);
+        addRow(table, "$(соруководительвкр.имяКратко)/…………..", fontSize, true);
+        addRow(table, "             (ФИО) / (подпись)", 8, false);
+        addRow(table, "«……» мая 2026 г.", fontSize, false);
+    }
 
-        XWPFTableRow row2 = removeAllCells(table.createRow());
-        row2.createCell();
-        XWPFTableCell row2Cell2 = row2.createCell();
-        XWPFRun run2 = addTextInCell(row2Cell2, "$(соруководительВКР.степень) $(соруководительВКР.звание)");
-        run2.setTextHighlightColor("yellow");
+    private XWPFRun addRow(XWPFTable table, String text, int fontSize, boolean isColored) {
+        XWPFTableRow row = removeAllCells(table.createRow());
+        row.createCell();
+        XWPFTableCell cell2 = row.createCell();
+        XWPFRun run = addTextInCell(cell2, text, fontSize);
+        if (isColored) {
+            run.setTextHighlightColor("yellow");
+        }
+        XWPFParagraph paragraph = (XWPFParagraph) run.getParent();
+        paragraph.setAlignment(ParagraphAlignment.LEFT);
+        paragraph.setIndentationFirstLine(0);
 
-        XWPFTableRow row3 = removeAllCells(table.createRow());
-        row3.createCell();
-        XWPFTableCell row3Cell2 = row3.createCell();
-        XWPFRun run3 = addTextInCell(row3Cell2, "$(соруководительВКР.должность.работаНГУ)");
-        run3.setTextHighlightColor("yellow");
-
-        XWPFTableRow row4 = removeAllCells(table.createRow());
-        row4.createCell();
-        XWPFTableCell row4Cell2 = row4.createCell();
-        XWPFRun run4 = addTextInCell(row4Cell2, "$(соруководительвкр.имяКратко)/…………..");
-        run4.setTextHighlightColor("yellow");
-
-        XWPFTableRow row5 = removeAllCells(table.createRow());
-        row5.createCell();
-        XWPFTableCell row5Cell2 = row5.createCell();
-        addTextInCell(row5Cell2, "             (ФИО) / (подпись)");
-
-        XWPFTableRow row6 = removeAllCells(table.createRow());
-        row6.createCell();
-        XWPFTableCell row6Cell2 = row6.createCell();
-        addTextInCell(row6Cell2, "«……» мая 2026 г.");
+        return run;
     }
 }
